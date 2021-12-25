cf76a47c5027802dbaf64abc7541d912cfa7a80f
philipk23 <philipk@abv.bg>
2021-12-25 18:56:19 +0200

    adding heather to home page

--- components/home.js
+++ components/home.js
@@ -1,7 +1,20 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+const template = () => html`
+    <div class="jumbotron jumbotron-fluid text-light" style="background-color: #343a40;">
+        <img src="https://s.studiobinder.com/wp-content/uploads/2019/06/Best-M-Night-Shyamalan-Movies-and-Directing-Style-StudioBinder.jpg"
+            class="img-fluid" alt="Responsive image">
+        <h1 class="display-4">Movies</h1>
+        <p class="lead">Unlimited movies, TV shows, and more. Watch anywhere. Cancel anytime.</p>
+    </div> 
+`
+
 export default class Home extends HTMLElement{
      connectedCallback(){
-         console.log('in home component');
+         this.render();
+     }
 
-         this.innerHTML = 'in home component'
+     render(){
+        render(template(), this);
      }
 }
