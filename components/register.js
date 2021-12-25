e2bc81a9f5d4fdaf89849cca4a8603c9fa5e70f3
philipk23 <philipk@abv.bg>
2021-12-25 19:32:14 +0200

    add form to template and submit event to register

--- components/register.js
+++ components/register.js
@@ -1,6 +1,54 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+const template = (ctx) => html`
+    <form class="text-center border border-light p-5" action="#" method="post" @submit=${ctx.onSubmit}>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" placeholder="Email" name="email" value="">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" placeholder="Password" name="password" value="">
+        </div>
+    
+        <div class="form-group">
+            <label for="repeatPassword">Repeat Password</label>
+            <input type="password" class="form-control" placeholder="Repeat-Password" name="repeatPassword" value="">
+        </div>
+    
+        <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+`;
+
 class Register extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = 'in register component'
+        this.render();
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        let formData = new FormData(e.target);
+
+        let email = formData.get('email');
+        let password = formData.get('password');
+        let repeatPassword = formData.get('repeatPassword');
+        
+        if (password != repeatPassword) {
+            console.error('password must match');
+            return;
+        }
+
+        if (password.length < 6) {
+            console.error('passwords must match');
+            return;
+        }
+
+        console.log('register successful');
+    }
+
+    render(){
+        render(template(this), this, { eventContext: this });
     }
 }
 
